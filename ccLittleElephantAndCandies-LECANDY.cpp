188d6efec0031a1873d04525b2c4ae0052c573da
gkushalg01 <57442239+gkushalg01@users.noreply.github.com>
2020-04-01 01:44:04 +0530

    Update ccLittleElephantAndCandies-LECANDY.cpp

--- ccLittleElephantAndCandies-LECANDY.cpp
+++ ccLittleElephantAndCandies-LECANDY.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+/////////////////// To make the code run faster by not syncing cin, cout anf ios to stdio ////////////////////
+static int __ = []() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+	return 0;
+}();
+//////////////////// -------------------------------------------------------------------- ////////////////////
 int main() {
     int t;
     cin>>t;
