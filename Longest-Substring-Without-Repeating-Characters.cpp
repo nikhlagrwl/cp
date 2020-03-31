dddafdf43e2516eb31db6b2e15384d14b37dbb4d
gkushalg01 <57442239+gkushalg01@users.noreply.github.com>
2020-04-01 04:30:16 +0530

    Create Longest-Substring-Without-Repeating-Characters.cpp

---
+++ Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,28 @@
+// contains only the solution function and some important test cases
+
+#define max(a,b) a>b?a:b
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> m;
+        int ans=0;
+        for(int i=0,j=0; i<s.size(); ++i){
+            if(m.find(s[i])!=m.end()){
+                j=max(m[s[i]],j);
+            }
+            ans=max(ans, i-j+1);
+            m[s[i]]=i+1;
+        }
+        return ans;
+    }
+};
+/*
+"abcabcbb"
+"bbbbb"
+" "
+""
+"dvdf"
+"vdvdf"
+"pwwkew"
+"abba"
+*/
