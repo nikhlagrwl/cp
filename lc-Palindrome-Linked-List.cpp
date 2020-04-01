0b329306418065900046d9437c4aed7cf0eb9ae0
gkushalg01 <57442239+gkushalg01@users.noreply.github.com>
2020-04-01 17:55:40 +0530

    Update lc-Palindrome-Linked-List.cpp

--- lc-Palindrome-Linked-List.cpp
+++ lc-Palindrome-Linked-List.cpp
@@ -1,1 +1,30 @@
+// CORRECT solution of the singly linked list palindrome problem
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+bool isPalindrome(ListNode* head) {
+    ListNode* p=head;
+    stack<int> s;
+    while(p!=NULL){
+        s.push(p->val);
+        p=p->next;
+    }
+    p=head;
+    int n=s.size()/2;
+    while(n--){
+        int k=s.top();
+        s.pop();
+        if(p->val!=k)
+            return false;
+        p=p->next;
+    }
+    return true;
+}
 
